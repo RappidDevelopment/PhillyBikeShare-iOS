0a345effb1089540242ae2664683e0628ab2988c
Matt Morgis <mattmorgis@me.com>
2015-05-23 17:46:33 -0400

    Initial commit

---
+++ Philly Bike Share/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Philly Bike Share
+//
+//  Created by Morgis, Matthew (ELS-PHI) on 5/23/15.
+//  Copyright (c) 2015 Rappid Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Philly Bike ShareTests/Philly_Bike_ShareTests.m
@@ -0,0 +1,40 @@
+//
+//  Philly_Bike_ShareTests.m
+//  Philly Bike ShareTests
+//
+//  Created by Morgis, Matthew (ELS-PHI) on 5/23/15.
+//  Copyright (c) 2015 Rappid Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Philly_Bike_ShareTests : XCTestCase
+
+@end
+
+@implementation Philly_Bike_ShareTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
