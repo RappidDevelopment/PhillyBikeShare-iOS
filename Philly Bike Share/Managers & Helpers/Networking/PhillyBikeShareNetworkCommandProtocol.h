f987431a00bb3576f9358038d54316f55380ed30
Matt Morgis <mattmorgis@me.com>
2015-05-24 12:46:19 -0400

    Adding a networking protocol

---
+++ Philly Bike Share/Managers & Helpers/Networking/PhillyBikeShareNetworkCommandProtocol.h
@@ -0,0 +1,15 @@
+//
+//  PhillyBikeShareNetworkCommandProtocol.h
+//  Philly Bike Share
+//
+//  Created by Morgis, Matthew on 5/24/15.
+//  Copyright (c) 2015 Rappid Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol PhillyBikeShareNetworkCommandProtocol <NSObject>
+
+- (void)execute;
+
+@end
