b6f042420793bef6cf377372f6bf36a8746594da
Matt Morgis <mattmorgis@me.com>
2015-05-24 16:29:07 -0400

    Creating the main view controller for the app and linking it to storyboard

---
+++ Philly Bike Share/Controllers/PhillyBikeShareMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhillyBikeShareMainViewController.h
+//  Philly Bike Share
+//
+//  Created by Morgis, Matthew on 5/24/15.
+//  Copyright (c) 2015 Rappid Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhillyBikeShareMainViewController : UIViewController
+
+@end
---
+++ Philly Bike Share/Controllers/PhillyBikeShareMainViewController.m
@@ -0,0 +1,37 @@
+//
+//  PhillyBikeShareMainViewController.m
+//  Philly Bike Share
+//
+//  Created by Morgis, Matthew on 5/24/15.
+//  Copyright (c) 2015 Rappid Development. All rights reserved.
+//
+
+#import "PhillyBikeShareMainViewController.h"
+
+@interface PhillyBikeShareMainViewController ()
+
+@end
+
+@implementation PhillyBikeShareMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
